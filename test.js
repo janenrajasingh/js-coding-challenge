870a7d6b92c1c32f54627b9a22eda688548313a3
janenrajasingh <69906174+janenrajasingh@users.noreply.github.com>
2020-08-19 22:38:07 +0530

    Added Comments

--- test.js
+++ test.js
@@ -1,16 +1,28 @@
 const { customSorting } = require("./sorting");
 const { getPermutations } = require("./permutations");
 
+/**
+ * Testing Permutations
+ */
 describe("Unique Permutations Function", () => {
   test("Returns array of n length contains all permuatations of given array", () => {
+    // Expected Permutations - [1,2], [2,1]
     expect(getPermutations([1, 2])).toHaveLength(2);
+
+    // Expected Permutations - [1,2,3], [2,1,3], [3,2,1], [1,3,2], [3,1,2], [2,3,1]
     expect(getPermutations([1, 2, 3])).toHaveLength(6);
   });
 });
 
+/**
+ * Testing Odd Even Sorting
+ */
 describe("Odd Even Numbers Custom Sorting", () => {
   test("Returns sorted array by custom odd even logics", () => {
+    // Odd - 1,3,7 ,Even - 2,4,16
     expect(customSorting([1, 2, 3, 4, 7, 16])).toEqual([1, 3, 7, 2, 4, 16]);
+
+    // Odd - 3,5,6 ,Even - 2,18
     expect(customSorting([5, 3, 2, 7, 18])).toEqual([3, 5, 7, 2, 18]);
   });
 });
